4062b32a7b7b4e0b0aed725fbed209de3001393b
TorGuard Ops <ops@torguard.net>
2022-11-27 15:16:22 +0000

    Check Connected

--- update-repo-packages.sh
+++ update-repo-packages.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# If we are not connected to the internet, exit this updater
+is_connected() {
+    ping -q -c3 1.1.1.1 >/dev/null 2>&1
+    return $?
+}
+
+[ is_connected ] || exit 0
+
 # This script is used to update the packages in the repo
 opkg update
 [ $? -eq 0 ] && {
