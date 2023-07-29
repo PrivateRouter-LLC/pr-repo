431f35cf3437753c270d2eee1746aec2e1f1c38f
Jason Hawks <jason@torguard.net>
2023-07-29 13:38:02 -0400

    Cleanup and install tgopenvpn

--- update-repo-packages.sh
+++ update-repo-packages.sh
@@ -46,22 +46,12 @@ echo "src/gz privaterouter_repo https://repo.privaterouter.com" >> /etc/opkg/cus
 # Temp fix to remove v2raya repo until we can find a backup
 sed -i '/v2raya/d' /etc/opkg/customfeeds.conf
 
-# Always install our repo's public key to the router
-#log_say "Installing v2raya repo public key"
-#wget -qO /tmp/v2raya.pub https://osdn.net/projects/v2raya/storage/openwrt/v2raya.pub
-#opkg-key add /tmp/v2raya.pub
-#rm /tmp/v2raya.pub
-
-# Always update the repo
-#log_say "Add v2raya repo"
-#sed -i '/v2raya/d' /etc/opkg/customfeeds.conf 
-#echo "src/gz v2raya https://osdn.net/projects/v2raya/storage/openwrt/$(. /etc/openwrt_release && echo "$DISTRIB_ARCH")" >> /etc/opkg/customfeeds.conf
-
 # This script is used to update the packages in the repo
 opkg update
 [ $? -eq 0 ] && {
-    #log_say "Installing v2raya packages"
-    #opkg install v2raya iptables-mod-conntrack-extra iptables-mod-extra iptables-mod-filter iptables-mod-tproxy kmod-ipt-nat6 kmod-ipt-tproxy xray-core luci-app-v2raya
+
+    # Everyone gets these packages
+    opkg install tgopenvpn
 
     # If we are on a "mini" device, we don't need the docker package
     # We check for the existance of the /etc/pr-mini file to determine
@@ -69,7 +59,6 @@ opkg update
         ## INSTALL ROUTER APP STORE ##
         log_say "Installing Router App Store..."
         opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard tgwireguard
-        #opkg install luci-app-v2raya v2raya
 
         ## REMOVE PACKAGES INSTALLED BY ERROR ##
         opkg remove luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-filetransfer luci-app-docker-backup luci-app-nextcloud
@@ -84,6 +73,5 @@ opkg update
         opkg install tgappstore luci-app-wizard luci-app-simplex luci-app-photoprism luci-app-libreddit luci-app-nodered luci-app-diskman
         opkg install luci-app-syncthing luci-app-qbittorrentdocker luci-app-megamedia luci-app-whoogle luci-app-nfs luci-app-webtop luci-app-alltube
         opkg install luci-app-emby luci-app-joplin luci-app-bookstack luci-app-filebrowser luci-app-heimdall luci-app-seafile
-        opkg install luci-app-v2raya v2raya
     fi
 }
