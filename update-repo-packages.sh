b8d8e429e1b25039d6dcae2edf9ebd0ee17c8a3e
Jason Hawks <jason@torguard.net>
2023-09-03 23:02:02 -0400

    Update

--- update-repo-packages.sh
+++ update-repo-packages.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# Get the name of the script without the path
+SCRIPT_NAME=$(basename "$0")
+
+# Count the number of running instances of the script (excluding the current one)
+NUM_INSTANCES=$(pgrep -f "${SCRIPT_NAME}" | grep -v "$$" | wc -l)
+
+# If more than one instance is found, exit
+if [ "$NUM_INSTANCES" -gt 1 ]; then
+    log_say "${SCRIPT_NAME} is already running, exiting."
+    exit 1
+fi
+
 # Log to the system log and echo if needed
 log_say()
 {
     SCRIPT_NAME=$(basename "$0")
     echo "${SCRIPT_NAME}: ${1}"
     logger "${SCRIPT_NAME}: ${1}"
+    echo "${SCRIPT_NAME}: ${1}" >> "/tmp/${SCRIPT_NAME}.log"
 }
 
 # Command to wait for Internet connection
@@ -32,6 +45,12 @@ wait_for_opkg() {
 # Wait for opkg to finish
 wait_for_opkg
 
+log_say "Waiting for opkg update to succesfully run..."
+while ! opkg update >/tmp/opkg_update.log 2>&1; do
+    log_say "... Waiting for opkg update to succesfully run ..."
+    sleep 1
+done
+
 # Always install our repo's public key to the router
 log_say "Installing PrivateRouter repo public key"
 wget -qO /tmp/public.key https://repo.privaterouter.com/public.key
@@ -47,31 +66,29 @@ echo "src/gz privaterouter_repo https://repo.privaterouter.com" >> /etc/opkg/cus
 sed -i '/v2raya/d' /etc/opkg/customfeeds.conf
 
 # This script is used to update the packages in the repo
-opkg update
-[ $? -eq 0 ] && {
 
-    # Everyone gets these packages
-    opkg install tgopenvpn tgsstp tganyconnect
+# Everyone gets these packages
+opkg install tgopenvpn tgsstp tganyconnect
 
-    # If we are on a "mini" device, we don't need the docker package
-    # We check for the existance of the /etc/pr-mini file to determine
-    if [ -f /etc/pr-mini ]; then
-        ## INSTALL ROUTER APP STORE ##
-        log_say "Installing Router App Store..."
-        opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard tgwireguard
+# If we are on a "mini" device, we don't need the docker package
+# We check for the existance of the /etc/pr-mini file to determine
+if [ -f /etc/pr-mini ]; then
+    ## INSTALL ROUTER APP STORE ##
+    log_say "Installing Router App Store..."
+    opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard tgwireguard
 
-        ## REMOVE PACKAGES INSTALLED BY ERROR ##
-        opkg remove luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-filetransfer luci-app-docker-backup luci-app-nextcloud
-        opkg remove luci-app-jellyfin luci-app-homeassistant tgdocker kmod-veth uxc procd-ujail procd-ujail-console
-        opkg remove luci-app-simplex luci-app-photoprism luci-app-libreddit luci-app-nodered luci-app-diskman
-        opkg remove luci-app-syncthing luci-app-qbittorrentdocker luci-app-megamedia luci-app-whoogle luci-app-nfs luci-app-webtop luci-app-alltube
-        opkg remove luci-app-emby luci-app-joplin luci-app-bookstack luci-app-filebrowser luci-app-heimdall luci-app-seafile
-    else
-        log_say "Installing PrivateRouter Packages"
-        opkg install luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-docker-backup luci-app-shortcutmenu tgwireguard luci-app-nextcloud
-        opkg install luci-app-poweroff tgdocker kmod-veth uxc procd-ujail procd-ujail-console
-        opkg install tgappstore luci-app-wizard luci-app-diskman
-        opkg install luci-app-syncthing luci-app-qbittorrentdocker
-        opkg install luci-app-filebrowser kmod-igc
-    fi
-}
+    ## REMOVE PACKAGES INSTALLED BY ERROR ##
+    opkg remove luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-filetransfer luci-app-docker-backup luci-app-nextcloud
+    opkg remove luci-app-jellyfin luci-app-homeassistant tgdocker kmod-veth uxc procd-ujail procd-ujail-console
+    opkg remove luci-app-simplex luci-app-photoprism luci-app-libreddit luci-app-nodered luci-app-diskman
+    opkg remove luci-app-syncthing luci-app-qbittorrentdocker luci-app-megamedia luci-app-whoogle luci-app-nfs luci-app-webtop luci-app-alltube
+    opkg remove luci-app-emby luci-app-joplin luci-app-bookstack luci-app-filebrowser luci-app-heimdall luci-app-seafile
+else
+    log_say "Installing PrivateRouter Packages"
+    opkg install luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-docker-backup luci-app-shortcutmenu tgwireguard luci-app-nextcloud
+    opkg install luci-app-poweroff tgdocker kmod-veth uxc procd-ujail procd-ujail-console
+    opkg install tgappstore luci-app-wizard luci-app-diskman
+    opkg install luci-app-syncthing luci-app-qbittorrentdocker
+    opkg install luci-app-filebrowser kmod-igc
+fi
+
