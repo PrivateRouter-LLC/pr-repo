ab9c097684001ed84479591d2ac2dabe9b7f8847
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-09-24 20:25:37 -0400

    Update update-repo-packages.sh
    Added pr-mesh profile

--- update-repo-packages.sh
+++ update-repo-packages.sh
@@ -68,16 +68,26 @@ done
 
 # This script is used to update the packages in the repo
 
-# Everyone gets these packages
-opkg install tgopenvpn tgsstp tganyconnect
+# not everyone gets these packages
+# opkg install tgopenvpn tgsstp tganyconnect
 
-# If we are on a "mini" device, we don't need the docker package
-# We check for the existance of the /etc/pr-mini file to determine
+# If we are on a "mini" or "mesh" device, we don't need the docker package
+# We check for the existence of the /etc/pr-mini /etc/pr-mesh file to determine
+
+if [ -f /etc/pr-mesh ]; then
+    ## INSTALL MESH LEAN PROFILE ##
+    log_say "Installing Mesh Packages..."
+    opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard
+    opkg remove wpad wpad-basic wpad-basic-openssl wpad-basic-wolfssl wpad-wolfssl
+    opkg install wpad-mesh-openssl kmod-batman-adv batctl avahi-autoipd
+    opkg remove tgwireguard tgopenvpn tgsstp tganyconnect
+fi
+
 if [ -f /etc/pr-mini ]; then
     ## INSTALL ROUTER APP STORE ##
     log_say "Installing Router App Store..."
-    opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard tgwireguard
-
+    opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard tgwireguard tgopenvpn tgsstp tganyconnect
+    
     ## REMOVE PACKAGES INSTALLED BY ERROR ##
     opkg remove luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-filetransfer luci-app-docker-backup luci-app-nextcloud
     opkg remove luci-app-jellyfin luci-app-homeassistant tgdocker kmod-veth uxc procd-ujail procd-ujail-console
@@ -85,11 +95,13 @@ if [ -f /etc/pr-mini ]; then
     opkg remove luci-app-syncthing luci-app-qbittorrentdocker luci-app-megamedia luci-app-whoogle luci-app-nfs luci-app-webtop luci-app-alltube
     opkg remove luci-app-emby luci-app-joplin luci-app-bookstack luci-app-filebrowser luci-app-heimdall luci-app-seafile
 else
-    log_say "Installing PrivateRouter Packages"
+    log_say "Installing PrivateRouter Cloud Packages"
+	opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard
+    opkg remove wpad wpad-basic wpad-basic-openssl wpad-basic-wolfssl wpad-wolfssl
+    opkg install wpad-mesh-openssl kmod-batman-adv batctl avahi-autoipd
     opkg install luci-lib-taskd taskd tgappstore luci-lib-xterm luci-lib-fs luci-app-docker-backup luci-app-shortcutmenu tgwireguard luci-app-nextcloud
     opkg install luci-app-poweroff tgdocker kmod-veth uxc procd-ujail procd-ujail-console
     opkg install tgappstore luci-app-wizard luci-app-diskman
     opkg install luci-app-syncthing luci-app-qbittorrentdocker
-    opkg install luci-app-filebrowser kmod-igc
+    opkg install luci-app-filebrowser kmod-igc tgopenvpn tgsstp tganyconnect
 fi
-
